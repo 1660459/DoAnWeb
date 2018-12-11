6004c20e7c559f63fd175e99b57349533164df90
1660459 <bichphuong0452@gmail.com>
2018-12-12 02:05:13 +0700

    update

---
+++ dataProvider.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+// PDO : php data ojects
+try
+{
+	$conn = new PDO('mysql:host=localhost;dbname=db;charset=utf8', 'root', '');
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+	
+catch(PDOException $e)
+ {
+    echo "Connection failed: " . $e->getMessage();
+ }
---
+++ module/functions.php
@@ -0,0 +1,83 @@
+<?php
+    function SelectAllProduct()
+    {
+        global $conn;
+        $stmt = $conn-> prepare("SELECT * FROM sanpham");
+        $stmt->execute();
+	    $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	    return $products;
+    }
+
+    function NewArrivals()
+    {
+        global $conn;
+        $stmt = $conn-> prepare("SELECT * FROM sanpham as `sp`  ORDER BY `sp`.`NgayTiepNhan` DESC  LIMIT 0 , 12");
+        $stmt->execute();
+	    $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	    return $products;
+    }
+
+    function BestSeller()
+    {
+        global $conn;
+        $stmt = $conn-> prepare("SELECT * FROM chitietdathang as `ctdh`  ORDER BY `ctdh`.`so_luong` DESC  LIMIT 0 , 10");
+        $stmt->execute();
+	    $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	    return $products;
+    }
+
+    function HightViewer()
+    {
+        global $conn;
+        $stmt = $conn-> prepare("SELECT * FROM sanpham as `sp`  ORDER BY `sp`.`luot_xem` DESC  LIMIT 0 , 10");
+        $stmt->execute();
+	    $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	    return $products;
+    }
+
+    function SelectCategoryProduct()
+    {
+        global $conn;
+        $stmt = $conn-> prepare("SELECT * FROM danhmuc ");
+        $stmt->execute();
+	    $cateproducts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $cateproducts;
+        
+    }
+    function SelectProductsByCategory($id)
+    {
+        global $conn;
+        $stmt = $conn-> prepare("SELECT * FROM sanpham as `sp` WHERE `sp`.`DanhMuc_idDanhMuc` = ? ");
+        $stmt->execute(array($id));
+	    $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $products;
+    }
+
+    function SelectProductById($id)
+    {
+        global $conn;
+        $stmt = $conn-> prepare("SELECT * FROM sanpham as `sp` WHERE `sp`.`id_sp` = ? ");
+        $stmt->execute(array($id));
+	    $products = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $products;
+    }
+
+    function SelectProducer()
+    {
+        global $conn;
+        $stmt = $conn-> prepare("SELECT * FROM nsx ");
+        $stmt->execute();
+	    $producer = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $producer;
+    }
+
+    function SelectProductByProducer($id)
+    {
+        global $conn;
+        $stmt = $conn-> prepare("SELECT * FROM sanpham as `sp` WHERE `sp`.`NSX_idNSX` = ? ");
+        $stmt->execute(array($id));
+	    $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $products;
+    }
+?>
+
